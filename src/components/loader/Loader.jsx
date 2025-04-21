e2ab72c0739c879f47353b202a27530cc2338a5f
Jerem16 <jeremy.lemaignent@gmail.com>
2025-04-21 14:48:00 +0200

    features add pages (empty)

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import dynamic from "next/dynamic";
+import "./loader.css"
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <div className="ld-circleBG"></div>
+            <div className="ld-frame">
+                {/* <img src="/img/retroviseur.svg" alt="loader" />{" "} */}
+                <span className="ld-dot2"></span>
+                <span className="ld-dot1"></span>
+                <span className="ld-dot"></span>
+            </div>{" "}
+            <p>
+                Cette section n’est pas encore disponible. Merci de votre
+                patience
+            </p>
+        </div>
+    );
+};
+
+export default dynamic(() => Promise.resolve(React.memo(Loader)));
