c5abfc231f80034a6dec1a04ffd6803e4673d3d5
Jerem16 <jeremy.lemaignent@gmail.com>
2026-01-28 18:19:19 +0100

    start

--- src/features/mobile-nav/components/MenuOpen.tsx
+++ src/features/mobile-nav/components/MenuOpen.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, memo, useEffect } from "react";
+import { useCallback, useMemo, memo } from "react";
 import { useNavigation } from "../utils/context/NavigationContext";
 import MenuList from "./MenuList";
 import type { MenuItem } from "../data/menuItems";
@@ -11,8 +11,7 @@ interface NavProps {
 }
 
 const MenuOpen = ({ menuItems, onNavigationClick }: NavProps) => {
-    const { hamburgerMenuIsOpen, openSubMenu, setOpenSubMenu, closeHamburgerMenu } =
-        useNavigation();
+    const { hamburgerMenuIsOpen, openSubMenu, setOpenSubMenu } = useNavigation();
 
     const handleMenuClick = useCallback(
         (menuItemId: string) => {
@@ -26,57 +25,17 @@ const MenuOpen = ({ menuItems, onNavigationClick }: NavProps) => {
         [hamburgerMenuIsOpen]
     );
 
-    const overlayClass = useMemo(
-        () => `mnav__overlay ${hamburgerMenuIsOpen ? "open" : ""}`,
-        [hamburgerMenuIsOpen]
-    );
-
-    const handleClose = useCallback(() => {
-        closeHamburgerMenu(1);
-    }, [closeHamburgerMenu]);
-
-    useEffect(() => {
-        if (hamburgerMenuIsOpen) {
-            document.body.classList.add("mnav--open");
-        } else {
-            document.body.classList.remove("mnav--open");
-        }
-
-        return () => {
-            document.body.classList.remove("mnav--open");
-        };
-    }, [hamburgerMenuIsOpen]);
-
-    useEffect(() => {
-        if (!hamburgerMenuIsOpen) return;
-
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.key === "Escape") {
-                closeHamburgerMenu(1);
-            }
-        };
-
-        window.addEventListener("keydown", handleKeyDown);
-        return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [hamburgerMenuIsOpen, closeHamburgerMenu]);
-
     return (
-        <>
-            <div className={overlayClass} onClick={handleClose} aria-hidden="true" />
-            <div className={containerClass} role="dialog" aria-modal="true" aria-label="Menu">
-                <button className="mnav__close" type="button" onClick={handleClose}>
-                    Fermer
-                </button>
-                {hamburgerMenuIsOpen && menuItems.mainLink && (
-                    <MenuList
-                        menuItems={menuItems.mainLink}
-                        openSubMenu={openSubMenu}
-                        onNavigationClick={onNavigationClick}
-                        handleMenuClick={handleMenuClick}
-                    />
-                )}
-            </div>
-        </>
+        <div className={containerClass}>
+            {hamburgerMenuIsOpen && menuItems.mainLink && (
+                <MenuList
+                    menuItems={menuItems.mainLink}
+                    openSubMenu={openSubMenu}
+                    onNavigationClick={onNavigationClick}
+                    handleMenuClick={handleMenuClick}
+                />
+            )}
+        </div>
     );
 };
 
