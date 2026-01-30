ab2630cf58e072590ff6789377704e59c5ae4eff
Jerem16 <jeremy.lemaignent@gmail.com>
2026-01-30 12:09:05 +0100

    start test 2

--- src/components/00-Header/Logo.jsx
+++ src/components/00-Header/Logo.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useId } from "react";
 import Link from "next/link";
-
 const LazyMyLogoW = React.lazy(() => import("../99-Svg_Icon/MyLogoW"));
 const LazyMyLogoBG = React.lazy(() => import("../99-Svg_Icon/MyLogoBG"));
 const LazyMyLogoTypo = React.lazy(() => import("../99-Svg_Icon/MyLogoTypo"));
 
 const Logo = () => {
+    const raw = useId();
+    const idPrefix = `logo-${raw.replace(/[^a-zA-Z0-9_-]/g, "")}`;
     // Title prop with a default value
     return (
-        <Link className="logo" href="/" title="Aller à la page d'accueil">
+        <Link
+            className="logo"
+            data-logo-id={idPrefix}
+            href="/"
+            title="Aller à la page d'accueil"
+        >
             <img src="/img/logo1.svg" className="my-logo l1" alt="Logo-menu" />
-            <LazyMyLogoBG />
-            <LazyMyLogoTypo />
-            <LazyMyLogoW />
-
+            <LazyMyLogoBG idPrefix={idPrefix} />
+            <LazyMyLogoTypo idPrefix={idPrefix} />
+            <LazyMyLogoW idPrefix={idPrefix} />
         </Link>
     );
 };
