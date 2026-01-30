020ad9a33933cf81aea53217d75590b3c9c1409d
Jérémy Lemaignent <94492716+Jerem16@users.noreply.github.com>
2026-01-30 01:06:44 +0100

    Fix prefixer les defs du logo

--- src/components/99-Svg_Icon/MyLogoBG.jsx
+++ src/components/99-Svg_Icon/MyLogoBG.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useId } from "react";
+import LogoDefs from "./LogoDefs";
 import SideBodyLogo from "./SideBodyLogo";
 const MyLogoBG = () => {
+    const baseId = useId().replace(/:/g, "");
+    const prefix = `logo-${baseId}`;
     return (
         <SideBodyLogo className={"logoBG"}>
             <path
-                id="Z"
+                id={`${prefix}-Z`}
                 opacity=".5"
                 d="M235 470L0 235 235 0 470 235z"
                 className="animated-z"
             />
+            <LogoDefs prefix={prefix} />
             <use
-                href="#Z"
-                fill="url(#A)"
+                href={`#${prefix}-Z`}
+                fill={`url(#${prefix}-A)`}
                 style={{ mixBlendMode: "screen" }}
-                filter="url(#H)"
+                filter={`url(#${prefix}-H)`}
             />
         </SideBodyLogo>
     );
