2ce74199085aa5bb2f7830796f13f49d8ae0aafb
Jérémy Lemaignent <94492716+Jerem16@users.noreply.github.com>
2026-02-03 11:52:58 +0100

    Refactor mobile nav providers

---
+++ src/features/mobile-nav/components/MobileNavWithProviders.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { memo } from "react";
+import MobileNav from "./MobileNav";
+import { NavigationProvider } from "@/features/navigation/core/context/NavigationContext";
+import { ScrollProvider } from "@/features/navigation/core/context/ScrollContext";
+
+const MobileNavWithProviders = () => {
+    return (
+        <NavigationProvider>
+            <ScrollProvider>
+                <MobileNav />
+            </ScrollProvider>
+        </NavigationProvider>
+    );
+};
+
+export default memo(MobileNavWithProviders);
