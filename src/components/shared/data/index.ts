a64e76c75025cdb10cb5018ba9591c205cf964d9
Jérémy Lemaignent <94492716+Jerem16@users.noreply.github.com>
2026-02-10 20:22:19 +0100

    chore: remove validated unused UZ files

--- src/components/shared/data/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as navLinks } from "@/assets/data/navLinks.json";
-export { menuItems } from "@/features/mobile-nav/data/menuItems";
-export type { MenuItem } from "@/features/mobile-nav/data/menuItems";
